90dfc977e6c812ae120f0d8c5fcf6c135d4e1c4f
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2025-05-03 14:41:21 +0300

    IRResources

---
+++ IRResources/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  IRResources
+//
+//  Created by Ömer Faruk Öztürk on 3.05.2025.
+//
+
+import Foundation
---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IRResources",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v18)],
+    products: [
+        .library(
+            name: "IRResources",
+            targets: ["IRResources"]
+        ),
+    ],
+    dependencies: [
+        
+    ],
+    targets: [
+        .target(
+            name: "IRResources",
+            dependencies: [
+                
+            ],
+            path: "IRResources"
+        ),
+    ]
+)
